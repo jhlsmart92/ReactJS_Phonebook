6b14cd459756c8dc3b3dda5341b633aceff65e9b
DESKTOP-0TCN8ST\jhlsm <jhlsmart92@gmail.com>
2020-10-21 02:17:45 +0900

    input

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PhoneForm from './components/PhoneForm';
+import './App.css';
+
+function App() {
+  return (
+    <div className = "App" >
+      <PhoneForm/>
+    </div>
+
+  );
+}
+
+export default App;
