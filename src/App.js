beb1469d632add4f9de742f566c1d181945e778c
DESKTOP-0TCN8ST\jhlsm <jhlsmart92@gmail.com>
2020-10-21 20:27:07 +0900

    array rendering

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import PhoneForm from './components/PhoneForm';
 import './App.css';
+import PhoneInfoList from './components/PhoneInfoList';
 
 class App extends Component {
   
@@ -23,7 +24,7 @@ class App extends Component {
     return (
       <div>
         <PhoneForm onCreate = {this.handleCreate}/>
-        {JSON.stringify(this.state.information)}
+        <PhoneInfoList data = {this.state.information} />
       </div>
     );
 
